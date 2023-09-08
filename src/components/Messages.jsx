9e659e4a1ce1e3863d43014509309953e7214421
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-09-08 23:31:00 +0300

    first commit

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-
 import styles from "../styles/Messages.module.css";
 
 const Messages = ({ messages, name }) => {
   return (
     <div className={styles.messages}>
-      {messages.map(({ user, message }, i) => {
+      {messages.map(({ user, message, image }, i) => {
         const itsMe =
           user.name.trim().toLowerCase() === name.trim().toLowerCase();
         const className = itsMe ? styles.me : styles.user;
 
         return (
           <div key={i} className={`${styles.message} ${className}`}>
-            <span className={styles.user}>{user.name}</span>
+            <span className={styles.username}>{user.name}</span>
 
-            <div className={styles.text}>{message}</div>
+            {message && (
+              <div className={styles.text}>{message}</div>
+            )}
+
+            {image && (
+              <img
+                src={image}
+                alt={`${user.name}'s image`}
+                className={styles.image}
+              />
+            )}
           </div>
         );
       })}
